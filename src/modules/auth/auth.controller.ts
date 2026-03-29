249f7bdbb9a3f9c132ee76ded9f99a0022f2c0db
vincent cupid tabiongan <cupidvincent@gmail.com>
2026-03-29 18:32:44 +0800

    swagger

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -13,10 +13,17 @@ import {
     Request,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import { CreateAuthDto, LoginDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { LocalAuthGuard } from '../../guards/local-auth.guard';
 import { JwtRefreshGuard } from '../../guards/jwt-refresh.guard';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
+// import {
+//     ApiCookieAuth,
+//     ApiOperation,
+//     ApiResponse,
+//     ApiTags,
+// } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -29,7 +36,11 @@ export class AuthController {
 
     @UseGuards(LocalAuthGuard)
     @Post('login')
-    async login(@Request() req, @Res({ passthrough: true }) res) {
+    async login(
+        @Body(ValidationPipe) body: LoginDto,
+        @Request() req,
+        @Res({ passthrough: true }) res,
+    ) {
         const { access_token, refresh_token, ...rest } =
             await this.authService.login(req.user, req.body.password);
 
@@ -45,8 +56,18 @@ export class AuthController {
         return rest;
     }
 
+    // @ApiTags('Auth')
     @UseGuards(JwtRefreshGuard)
     @Post('refresh')
+    // @ApiOperation({ summary: 'Refresh access token using refresh token' })
+    // @ApiCookieAuth('refresh_token')
+    // @ApiResponse({
+    //     status: 200,
+    //     description: 'Tokens refreshed successfully',
+    //     schema: {
+    //         example: { success: true },
+    //     },
+    // })
     async refresh(@Req() req, @Res({ passthrough: true }) res) {
         const { access_token, refresh_token, user } =
             await this.authService.refreshTokens(
@@ -60,21 +81,25 @@ export class AuthController {
         return { success: true };
     }
 
+    @ApiExcludeEndpoint()
     @Get()
     findAll() {
         return this.authService.findAll();
     }
 
+    @ApiExcludeEndpoint()
     @Get(':id')
     findOne(@Param('id') id: string) {
         return this.authService.findOne(+id);
     }
 
+    @ApiExcludeEndpoint()
     @Patch(':id')
     update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
         return this.authService.update(+id, updateAuthDto);
     }
 
+    @ApiExcludeEndpoint()
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.authService.remove(+id);
--- src/modules/auth/dto/create-auth.dto.ts
+++ src/modules/auth/dto/create-auth.dto.ts
@@ -1,14 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateAuthDto {
+    @ApiProperty()
     @IsString()
     firstName: string;
 
+    @ApiProperty()
     @IsString()
     lastName: string;
+
+    @ApiProperty()
     @IsString()
     email: string;
 
+    @ApiProperty()
+    @IsString()
+    password: string;
+}
+
+export class LoginDto {
+    @ApiProperty()
+    @IsString()
+    email: string;
+
+    @ApiProperty()
     @IsString()
     password: string;
 }
